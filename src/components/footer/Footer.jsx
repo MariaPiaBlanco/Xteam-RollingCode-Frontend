03a394ad9aab1ed061ca5d48f6030f5ec519441c
alevexler <alevexler2@gmail.com>
2022-07-25 11:20:19 -0300

    create foote mobile interface

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,17 +9,17 @@ const Footer = () => {
       <div className="row">
         <div className={`container-fluid col-12 ${footerBgDark}`}>
           <div className="row justify-content-evenly">
-            <div className="col-4">
+            <div className="col-12 col-lg-4 text-center">
               <img src={Logo} alt="xTeamLogo" className="w-50" />
             </div>
-            <div className="d-flex flex-column col-2 align-items-center mt-2">
+            <div className="d-flex flex-column col-12 col-lg-2 align-items-center mt-2">
               <span>Home</span>
               <span>Destacado</span>
               <span>Contacto</span>
               <span>Favortio</span>
             </div>
-            <div className="col-4">
-              <form className=" d-flex justify-content-end align-items-center h-100" role="search">
+            <div className="col-4 d-none d-sm-block ">
+              <form className="d-flex justify-content-center align-items-center h-100" role="search">
                 <div className="input-group mb-3 p-2 w-50">
                   <input autoComplete="off" type="password" className={`form-control ${inputBorder}`} placeholder="Repetir Password" aria-label="password" aria-describedby="basic-addon1"/>
                   <button className={`input-group-text ${inputBorder} ${inputBg}`} id="basic-addon1"><box-icon name="search" color="#ffffff" ></box-icon></button>
@@ -29,7 +29,7 @@ const Footer = () => {
           </div>
         </div>
         <div className={`col-12 ${footerBgDarkLight} text-center`}>
-          <span>Copyright © 2022 xTeam - All rights reserved</span>
+          <h6 className="pt-2">Copyright © 2022 xTeam - All rights reserved</h6>
         </div>
       </div>
     </section>
