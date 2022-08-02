048e202e6784a8511923c5bd47a9a4aeac706edf
Marcelo <marcelo.s44@gmail.com>
2022-08-01 23:41:47 -0500

    Review styles header secction

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import logoImg from "../../assets/Logo.jpeg";
+import logoImg from "../../assets/xteam.png";
 import searchImg from "../../assets/search.svg";
 import cartImg from "../../assets/cart.svg";
 import questImg from "../../assets/question.svg";
-import styles from "./navbar.module.css";
+import styles from "./navbar_module.css";
 import { NavLink, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleQuestion, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const {
@@ -18,13 +20,13 @@ const Navbar = () => {
   } = styles;
   const navigate = useNavigate();
   return (
-    <nav className={`navbar navbar-expand-lg navbar-light ${navXteam}`}>
+    <nav className={`sticky-top navbar navbar-expand-lg container_navbar--top ${navXteam}`}>
       <div className="container-fluid d-flex justify-content-between">
         <img
           onClick={() => navigate("/", { replace: true })}
-          className="col-9 col-lg-2 my-1"
+          className="col-9 col-lg-2 m-1 logo-img"
           src={logoImg}
-          alt="Logo"
+          alt="Xteam"
         />
         <button
           className="navbar-toggler me-3 border-0 btn-burger"
@@ -37,7 +39,7 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="container">
+        <div className="container container_navbar--items">
           <div
             className="collapse navbar-collapse d-flex justify-content-between row"
             id="navbarScroll"
@@ -54,39 +56,39 @@ const Navbar = () => {
                 </NavLink>
               </li>
               <li className="nav-item px-2">
-                <NavLink  className={({ isActive }) =>
-                    isActive ? `${navigateLinkActive}` : `${navigateLink}`
-                  } to="/contact">
+                <NavLink className={({ isActive }) =>
+                  isActive ? `${navigateLinkActive}` : `${navigateLink}`
+                } to="/contact">
                   Contacto
                 </NavLink>
               </li>
               <li className="nav-item px-2">
-                <NavLink  className={({ isActive }) =>
-                    isActive ? `${navigateLinkActive}` : `${navigateLink}`
-                  } to={"/Favorites"}>
+                <NavLink className={({ isActive }) =>
+                  isActive ? `${navigateLinkActive}` : `${navigateLink}`
+                } to={"/Favorites"}>
                   Favoritos
                 </NavLink>
               </li>
             </ul>
             <div className="d-flex me-2 col-10 col-lg-4 px-0">
               <input
-                className={`form-control  text-center text-light ${inpSearch}`}
+                className={`form-control text-center text-light field__input-search ${inpSearch}`}
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
               <button
-                className={`btn btn-outline-secondary ${btnSearch}`}
+                className={`btn btn-outline-secondary field__input-search-icon ${btnSearch}`}
                 type="submit"
               >
                 {" "}
                 <img src={searchImg} alt="search" />
               </button>
-              <button className={`border-0 mx-2 ${btnCart}`}>
-                <img src={cartImg} alt="cart"></img>
+              <button className={`border-0 mx-2 button-icon ${btnCart}`}>
+                <FontAwesomeIcon icon={faCartShopping} ></FontAwesomeIcon>
               </button>
-              <button className={`border-0 ${btnQuest}`}>
-                <img src={questImg} alt="quest"></img>
+              <button className={`border-0 button-icon ${btnQuest}`}>
+                <FontAwesomeIcon icon={faCircleQuestion} ></FontAwesomeIcon>
               </button>
             </div>
           </div>
