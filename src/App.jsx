0488f404af1a0f382bca808f232125db5ed634b5
flo270 <flomedina07@gmail.com>
2022-08-11 19:44:33 -0300

    adding component inicio and page inicio- video presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Register from "./pages/register/Register.jsx";
 import Login from "./pages/login/Login.jsx";
@@ -12,15 +13,21 @@ import AdminPage from "./pages/adminPage/AdminPage.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import InicioPage from '../src/pages/inicioPage'
 
 const App = () => {
+  const [videoLoader, setVideoLoader] = useState(true)
+  setTimeout(()=>{
+   setVideoLoader(false)
+  },10500)
+
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <SocialContact />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={ videoLoader ? <InicioPage /> : <HomePage />} />
           <Route path="/highlightpage/:id" element={<HighLightPage />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/contact" element={<ContactPage />} />
