d69780fa172ec952cccc5dfba27853de0086ea0e
Marcelo <marcelo.s44@gmail.com>
2022-08-12 10:48:20 -0500

    Card home fixed styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,29 +5,36 @@ import SubmitButton from "../submitButton/SubmitButton";
 
 const Card = ({ game }) => {
   const navigate = useNavigate();
-  const {image, title, details, price, _id} = game;
-  const { productGame, imgCard, pCard } = styles;
+  const { image, title, details, price, _id } = game;
+  const { productGame, imgCard, pCard, gameDetails } = styles;
   const btnMensage1 = "Ver detalle del producto";
   const btnMensage2 = "En el carrito";
 
   return (
-    <div className={`card col-lg-3 col-sm-6 mx-1 my-3 py-2 ${productGame}`} onClick={()=>navigate(`/highlightpage/${_id}`)} >
+    <div className={`card col-lg-3 col-sm-6 mx-1 my-3 py-2 ${productGame}`} onClick={() => navigate(`/highlightpage/${_id}`)} >
       <img
         src={image[0]}
         className={`card-img-top ${imgCard}`}
         alt={title}
       />
       <div className="card-body">
-        <h3 className={`${pCard} text-light`}>
-          {title} - ${price}
-        </h3>
-        <p className={`card-text text-light fs-5`}>{details}</p>
+        <h4 className={`${pCard} text-light`}>
+          {title}
+        </h4>
+        <h5 className={`${pCard} text-light`}>
+          ${price}
+        </h5>
+
+        <p className={`card-text text-light fs-6 mt-1 ${gameDetails}`}>{details}</p>
       </div>
-      {!game.inCart ? (
-      <SubmitButton mensage={btnMensage1} />
-          ) : (
-          <SubmitButton  mensage={btnMensage2} />
+
+      <div className={`card-text text-light fs-6 mt-1 mb-2 text-center `}>
+        {!game.inCart ? (
+          <SubmitButton mensage={btnMensage1} />
+        ) : (
+          <SubmitButton mensage={btnMensage2} />
         )}
+      </div>
     </div>
   );
 };
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState }from 'react'
+import React, { useEffect, useState } from 'react'
 import Carousel from '../../components/carousel/Carousel.jsx';
 import Card from '../../components/card/Card';
 import Adds from '../../components/adds/Adds';
@@ -7,28 +7,29 @@ import axios from "axios";
 import styles from './homePage.module.css';
 
 function HomePage() {
-  const {gameCard} = styles;
+  const { gameCard } = styles;
 
   const [games, setGames] = useState([])
-  const getData = async () =>{ 
+  const getData = async () => {
     await axios.get(`${process.env.REACT_APP_URL_BASE}/games`)
-      .then((response)=>setGames(response.data))
-  } 
+      .then((response) => setGames(response.data))
+  }
   useEffect(() => {
     getData()
   }, [])
   return (
     <div>
-      <Carousel/>
+      <Carousel />
       <div className='container-fluid'>
         <div className={`row justify-content-evenly m-0 ${gameCard}`}>
-          {games?.map(game=><Card game={game} key={game._id}/>)}
+          {games?.map(game => <Card game={game} key={game._id} />)}
         </div>
       </div>
-      <Adds/>
-      <WhoWeAre/>
+      <Adds />
+      <WhoWeAre />
     </div>
-  )  };
+  )
+};
 
 
 export default HomePage
